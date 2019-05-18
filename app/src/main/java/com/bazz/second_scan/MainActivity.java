917738278c5a34d624002d90723e3fd934da0436
Abbas <airekeola@byteworks.com.ng>
2019-05-18 12:48:06 +0100

    My first ever Scanner App Project

---
+++ app/src/main/java/com/bazz/second_scan/MainActivity.java
@@ -0,0 +1,59 @@
+package com.bazz.second_scan;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    TextView tvCardText;
+    Button btStartScan;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvCardText = (TextView)findViewById(R.id.tv_code_text);
+        btStartScan = (Button)findViewById(R.id.btn_scan);
+
+        btStartScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startQRScanner();
+            }
+        });
+
+    }
+    private void startQRScanner() {
+        new IntentIntegrator(this).initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result =   IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(this,    "Cancelled",Toast.LENGTH_LONG).show();
+            } else {
+                updateText(result.getContents());
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    private void updateText(String scanCode) {
+        tvCardText.setText(scanCode);
+    }
+
+
+}
+
